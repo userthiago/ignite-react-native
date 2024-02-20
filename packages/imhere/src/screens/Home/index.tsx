bf2373531aababb9e8cd526cd3aca7ad9a126a78
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-02-20 11:27:06 -0300

    feat(package/imhere): Adicionando formulário para adicionar participantes na homepage.

--- packages/imhere/src/screens/Home/index.tsx
+++ packages/imhere/src/screens/Home/index.tsx
@@ -1,11 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
+import { useState } from "react";
 
-export default function Home() {
+export function Home() {
+  const [participantList, setParticipantList] = useState([]);
+
+  const handleParticipantAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- packages/imhere/src/screens/Home/styles.ts
+++ packages/imhere/src/screens/Home/styles.ts
@@ -16,4 +16,36 @@ export const styles = StyleSheet.create({
     color: "#6B6B6B",
     fontSize: 16,
   },
+  input: {
+    flex: 1,
+
+    backgroundColor: "#1F1E25",
+    height: 56,
+    paddingHorizontal: 16,
+    borderRadius: 4,
+
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 24,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+
+    backgroundColor: "#31CF67",
+    width: 56,
+    height: 56,
+    borderRadius: 4,
+  },
+  form: {
+    flexDirection: "row",
+    gap: 8,
+
+    width: "100%",
+    marginTop: 36,
+    marginBottom: 42,
+  },
 });
