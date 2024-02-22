411702dd3ddeddafe8208b385a0112b54423414b
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-02-22 10:18:29 -0300

    feat: Implementando componente de Loading.

--- packages/igniteteams/App.tsx
+++ packages/igniteteams/App.tsx
@@ -1,12 +1,21 @@
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
 
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
