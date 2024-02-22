a853f52397270188bb5c918546d6dcea800e261e
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-02-22 15:40:30 -0300

    feat: Implementando componente de input.

--- packages/igniteteams/src/screens/NewGroup/index.tsx
+++ packages/igniteteams/src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Icon, NewGroupContainer, NewGroupContent } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -13,7 +14,8 @@ export function NewGroup() {
           title="Nova Turma"
           subtitle="crie uma turma para adicionar pessoas"
         />
-        <Button title="Criar" />
+        <Input placeholder="Nome da turma" />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </NewGroupContent>
     </NewGroupContainer>
   );
