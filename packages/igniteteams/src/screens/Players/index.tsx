ae4b117a0f89fd40619521a7d7c2938bb1b3347e
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-02-26 11:06:21 -0300

    feat: Inserindo filtro de times e contador de players na tela de Players.

--- packages/igniteteams/src/screens/Players/index.tsx
+++ packages/igniteteams/src/screens/Players/index.tsx
@@ -1,11 +1,20 @@
 import { Header } from "@components/Header";
-import { Form, PlayersContainer } from "./styles";
+import { Form, HeaderList, PlayersAmount, PlayersContainer } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
+  const handleChangeTeam = (team: string) => {
+    setTeam(team);
+  };
+
   return (
     <PlayersContainer>
       <Header showBackButton />
@@ -17,8 +26,21 @@ export function Players() {
         <Input placeholder="Nome do participante" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="TIME A" isActive />
-      <Filter title="TIME B" />
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => handleChangeTeam(item)}
+            />
+          )}
+          horizontal
+        ></FlatList>
+        <PlayersAmount>{players.length}</PlayersAmount>
+      </HeaderList>
     </PlayersContainer>
   );
 }
--- packages/igniteteams/src/screens/Players/styles.ts
+++ packages/igniteteams/src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const PlayersContainer = styled.View`
   flex: 1;
@@ -14,4 +14,20 @@ export const Form = styled.View`
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
   width: 100%;
   border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  width: 100%;
+  margin: 32px 0 12px;
+`;
+
+export const PlayersAmount = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-size: ${theme.FONT_SIZE.SM};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
 `;
