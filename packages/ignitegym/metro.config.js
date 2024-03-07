cb30b4337a8131113138c1b5c617acd6f7f36045
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-03-07 16:02:12 -0300

    chore: Configurando visualização de SVG dentro do projeto.

--- packages/ignitegym/metro.config.js
+++ packages/ignitegym/metro.config.js
@@ -13,8 +13,19 @@ const config = getDefaultConfig(projectRoot);
 config.watchFolders = [monorepoRoot];
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-    path.resolve(projectRoot, 'node_modules'),
-    path.resolve(monorepoRoot, 'node_modules'),
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
 ];
+
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
 
 module.exports = config;
