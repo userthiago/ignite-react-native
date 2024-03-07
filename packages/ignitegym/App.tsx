77b51ad93790c96885842c899d2046da820c5239
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-03-07 10:57:21 -0300

    feat: Adicionando e customizando a lib @gluestack-theme.

--- packages/ignitegym/App.tsx
+++ packages/ignitegym/App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar, Text } from "react-native";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed";
+import { Loading } from "@components/Loading";
+import { config } from "config/gluestack-ui.config";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <>
+    <GluestackUIProvider config={config}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Teste</Text> : <Text>Loading...</Text>}
-    </>
+      {!fontsLoaded ? (
+        <Box>
+          <Text>Open up App.js to start working on your app!</Text>
+        </Box>
+      ) : (
+        <Loading />
+      )}
+    </GluestackUIProvider>
   );
 }
