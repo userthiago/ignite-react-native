d52d2ddee0cbbb8cde021a0cf580e72da25c6801
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-03-08 15:42:16 -0300

    feat: Implementando a tela de Signup.

--- packages/ignitegym/App.tsx
+++ packages/ignitegym/App.tsx
@@ -8,6 +8,7 @@ import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed";
 import { Loading } from "@components/Loading";
 import { config } from "config/gluestack-ui.config";
 import { SignIn } from "@screens/Signin";
+import { SignUp } from "@screens/Signup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </GluestackUIProvider>
   );
 }
