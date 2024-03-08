fd03c82bf91c23d6129dbf4f33210519ba63a9c0
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-03-08 11:11:00 -0300

    feat: Implementando input com suas devidas estilizações.

---
+++ packages/ignitegym/src/components/Input.tsx
@@ -0,0 +1,27 @@
+import { InputField, Input as GluestackInput } from "@gluestack-ui/themed";
+import { TextInputProps } from "react-native";
+
+export function Input({ ...rest }: TextInputProps) {
+  return (
+    <GluestackInput
+      bg="$gray700"
+      w="$full"
+      h="$14"
+      px="$3"
+      borderWidth={1}
+      borderColor="$gray700"
+      mb="$4"
+      $focus-backgroundColor="$gray700"
+      $focus-borderWidth={1}
+      $focus-borderColor="$green500"
+    >
+      <InputField
+        fontSize="$md"
+        color="$white"
+        fontFamily="$body"
+        placeholderTextColor="$gray300"
+        {...rest}
+      />
+    </GluestackInput>
+  );
+}
--- packages/ignitegym/src/screens/Signin.tsx
+++ packages/ignitegym/src/screens/Signin.tsx
@@ -1,16 +1,35 @@
 import { Image } from "react-native";
-import { VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
 
+import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="$gray700">
+    <VStack flex={1} bg="$gray700" px="$10">
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
         style={{ position: "absolute", top: 0 }}
       />
+      <Center my="$32">
+        <LogoSvg />
+        <Text color="$gray100" fontSize="$sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+      <Center>
+        <Heading color="$gray100" fontSize="$xl" mb="$6" fontFamily="$heading">
+          Acesse sua conta
+        </Heading>
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
+      </Center>
     </VStack>
   );
 }
