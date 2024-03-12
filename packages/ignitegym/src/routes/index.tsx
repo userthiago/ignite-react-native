956f109170cab17f6262cb28d08330af0a08e813
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-03-12 15:14:39 -0300

    feat: Configurando as rotas privadas com tab navigation.

--- packages/ignitegym/src/routes/index.tsx
+++ packages/ignitegym/src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { Box, useStyled } from "@gluestack-ui/themed";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 
-import { AuthRoutes } from "./auth.routes";
+// import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const styled = useStyled();
@@ -13,7 +14,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
